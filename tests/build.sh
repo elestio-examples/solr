fb7dcf6a4c4c2294cfe98da25a58ee9a29e831cc
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-07-17 13:56:55 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sed -i 's/\(SOLR_DOWNLOAD_SERVER\)/SOLR_DOWNLOAD_SERVER="solr.apache.org"/' 9.2/Dockerfile
 cp 9.2/* ./
-docker buildx build --build-arg SOLR_DOWNLOAD_SERVER=solr.apache.org . --output type=docker,name=elestio4test/solr:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/solr:latest | docker load
